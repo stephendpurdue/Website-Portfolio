0071e620589fcd85688a7648ef421c809e14f7de
Stephen <stephendpurdue@gmail.com>
2025-11-10 17:48:02 +0000

    Added Logo to homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,18 @@
-const Home = () => ('Hello')
+import LogoTitle from '../../assets/images/logo-s.png'
+
+const Home = () => {
+
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm</h1>
+                <img src={LogoTitle} alt="developer/" />
+            </div>
+
+        </div>
+ 
+    );
+}
 
 export default Home
