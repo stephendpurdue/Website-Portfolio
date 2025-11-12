738c4798b275ba00c50e1ffb41e25b3fcd05a6dc
Stephen <stephendpurdue@gmail.com>
2025-11-12 15:55:31 +0000

    Left side of about page completed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,21 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    useEffect(() => {
+    const timer = setTimeout(() => {
+        setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => clearTimeout(timer)
+}, [])
     return (
     <div className='container about-page'>
         <div className='text-zone'>
             <h1>
                 <AnimatedLetters 
+                letterClass={letterClass}
                     strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e','.']}
                     idx={15}
                 />
