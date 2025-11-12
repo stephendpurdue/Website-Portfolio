a99701501ce4d89bba705181a5ca6dc4178ff5c0
Stephen <stephendpurdue@gmail.com>
2025-11-12 09:28:14 +0000

    Started Logo on Homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo'
 import './index.scss';
 
 
@@ -38,7 +39,7 @@ const Home = () => {
                 <h2>Game Developer, University Student.</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-
+            <Logo />   
         </div>
  
     );
