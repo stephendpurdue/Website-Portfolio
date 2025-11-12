3e42482e67178992b6fda32dab75f314490c8eac
Stephen <stephendpurdue@gmail.com>
2025-11-12 14:20:03 +0000

    Removed Logo from Sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,12 @@
 // ...existing code...
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt="Logo" />
-            <img className="sub-logo" src={LogoSubtitle} alt="Purdue" />
-        </Link>
         <nav>
             <NavLink to="/" end className={({ isActive }) => (isActive ? 'active' : '')}>
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
