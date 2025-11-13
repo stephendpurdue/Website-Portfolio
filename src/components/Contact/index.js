9a375bed3c2b71d39a6dcb7a4f5b080b577b845b
Stephen <stephendpurdue@gmail.com>
2025-11-13 09:04:53 +0000

    Imported animated letters

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,22 @@
+import Loader from 'react-loaders'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
 
-const Contact = () => 'Hello'
+const Contact = () => {
+    
+    return (
+    <>
+        <div className='container contact-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters />
+                </h1>
+            </div>
+        </div>
+        
+        <Loader type="pacman"  />
+    </>
+    )
+}
 
 export default Contact
